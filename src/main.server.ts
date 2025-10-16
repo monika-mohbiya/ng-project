a4407c951f1f022d5a16d142c4f4c51fb85d43bb
Monika <monikamohbiya777@gmail.com>
2025-10-16 11:37:42 +0530

    Initial commit

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,33 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// import { config } from './app/app.config.server';
+
+// const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+// export default bootstrap;
+
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = (context: BootstrapContext | undefined) => bootstrapApplication(AppComponent, config, context);
 
 export default bootstrap;
+
+
+// import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// import { config } from './app/app.config.server';
+// import { provideServerRendering } from '@angular/platform-server';
+
+// const bootstrap = (context: BootstrapContext | undefined) => {
+//     return bootstrapApplication(AppComponent, {
+//         ...config,
+//         providers: [
+//             provideServerRendering(),
+//             ...(config.providers ?? []),
+//         ],
+//     }, context);
+// };
+
+// export default bootstrap;
